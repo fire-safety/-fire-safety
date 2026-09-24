3b2f70264a6334c7c1524b596b3f7efbed128564
fire-safety <matinaf04@gmail.com>
2026-09-24 10:39:02 +0300

    Hide loading text once a floor plan has loaded

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
       new version exists. If you forget, people keep seeing the old one.
    ============================================================ */
 
-const VERSION = 'fire-safety-v9';
+const VERSION = 'fire-safety-v10';
 
 const FILES = [
   './',
